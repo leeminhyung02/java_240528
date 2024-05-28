37e07b5ff2a6f485c0c1f27ed6c3ba4f30449af0
leeminhyung02 <mi087033@gmail.com>
2024-05-28 17:10:43 +0900

    자바 1일차 변수 예제

---
+++ java_lmh/src/day01/variableEX01.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class variableEX01 {
+
+	public static void main(String[] args) {
+		
+		char ch1;
+		char ch2 = 'A';
+		char ch3, ch4;
+		char ch5 = '+' , ch6 = '\\';
+		System.out.println(ch2);
+		System.out.println(ch6);
+		System.out.println(ch5);
+		
+		long num1 = 123456789012l;
+		float num2 =  3.14f;  //float에는 접미사f붙이기
+		double num3 = 3.14;
+		boolean isEven = true;
+	}
+
+}
