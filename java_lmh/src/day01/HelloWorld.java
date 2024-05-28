d085fc16f3a88da2cff4a68c1693ab314bf1dc6d
leeminhyung02 <mi087033@gmail.com>
2024-05-28 18:56:11 +0900

    주석 삭제- HelloWorld.java

--- java_lmh/src/day01/HelloWorld.java
+++ java_lmh/src/day01/HelloWorld.java
@@ -3,7 +3,6 @@ package day01;
 public class HelloWorld {
 	// 프로그램 실행을 위해 main 필요
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//콘솔에 원하는 문자를 출력하는 기능 ln은 줄바꿈
 		System.out.println("Hello World!");
 		// 한줄 주석
