ba1ae46047dda0e33f4232aeb5989d47f9a16a93
leeminhyung02 <mi087033@gmail.com>
2024-05-29 08:50:12 +0900

    수정

--- java_lmh/src/day01/HelloWorld.java
+++ java_lmh/src/day01/HelloWorld.java
@@ -3,7 +3,7 @@ package day01;
 public class HelloWorld {
 	// 프로그램 실행을 위해 main 필요
 	public static void main(String[] args) {
-		//콘솔에 원하는 문자를 출력하는 기능 ln은 줄바꿈
+		//콘솔에 원하는 문자를 출력하는 기능
 		System.out.println("Hello World!");
 		// 한줄 주석
 		/*
