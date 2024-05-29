466fa13da7af06325b3c5b8dc45816dff0106b50
leeminhyung02 <mi087033@gmail.com>
2024-05-29 09:50:55 +0900

    예제
    day2

---
+++ java_lmh/src/day02/variableEx03.java
@@ -0,0 +1,22 @@
+package day02;
+
+import java.util.Iterator;
+
+public class variableEx03 {
+	// 변수명 작성 규칙 예제
+	public static void main(String[] args) {
+		int num;
+		int NUM, nUm, nuM; // 대소문자를 구별하기 때문에 가능
+		
+		//int char, class; // 예약어 사용 불가능
+		
+		int _num, n_num, $num;
+		//  int n um, #num;    // 특수문자는 _, $ 만 가능
+		
+		int num1;
+		//int 1num; // 숫자는 앞에 올수 없다.
+		//int num;  // 중복 선언 불가능  현재 위치에서 사용중인 변수가 1개인 경우 사용 가능.   Ex 반복문 for
+		
+	}
+
+}
