0ba4c16ffb0a918730a1ff65510aa8771501bd62
leeminhyung02 <mi087033@gmail.com>
2024-05-29 10:13:39 +0900

    Update VariableEx04.java

--- java_lmh/src/day02/VariableEx04.java
+++ java_lmh/src/day02/VariableEx04.java
@@ -3,8 +3,15 @@ package day02;
 public class VariableEx04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Num대신 num 추천
+		int Num, num;
+		//totalcount대신 totalCount 추천
+		int totalcount, totaCount;
+		//상수는 대문자로 표현하는걸 추천
+		final int maxNum = 10, MAX_NUM = 10;
+		//나이를 의미하는 변수로 age 추천  변수명은 의미전달이 잘 되도록 작성
+		int age, a;
+		
 	}
 
 }
