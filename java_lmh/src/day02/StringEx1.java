c52be231333dd28eb92f5724015f38c4190f382b
leeminhyung02 <mi087033@gmail.com>
2024-05-29 09:36:19 +0900

    예제 실습
    day2

---
+++ java_lmh/src/day02/StringEx1.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		String str1 = null;
+		String str2 = "안녕하세요";
+		String str3 = "null";
+		String str4 = "";
+		
+		System.out.println(str1+"\n"+str2+"\n"+str3+"\n"+str4);
+
+	}
+
+}
---
+++ java_lmh/src/day02/variableEx02.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class variableEx02 {
+
+	public static void main(String[] args) {
+		int num1 = 0x11;  //16진수-17
+		int num2 = 0b11; //2진수-3
+		int num3 = 011;   //8진수-9
+		int num4 = 11;     //10진수-11
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		
+
+	}
+
+}
