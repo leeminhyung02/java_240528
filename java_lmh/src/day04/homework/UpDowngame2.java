dea9604bb9c9ca345829b904df7a4689b559ac98
leeminhyung02 <mi087033@gmail.com>
2024-05-31 17:41:57 +0900

    day04 homework

--- java_lmh/src/day04/homework/UpDowngame2.java
+++ java_lmh/src/day04/homework/UpDowngame2.java
@@ -2,7 +2,7 @@ package day04.homework;
 
 import java.util.Scanner;
 
-public class UpDowngame {
+public class UpDowngame2 {
 
 	public static void main(String[] args) {
 		// 1~100 사이의 랜덤한 수를 생성하여 해당 숫자를 맞추는 게임을 작성하세요
@@ -18,20 +18,16 @@ public class UpDowngame {
 		Scanner scan = new Scanner(System.in);
 		int num = 0;
 		int min = 1, max = 100; 
-		int random = get_random(min, max);
-		while (num != random) {
-			System.out.print("정수 입력 :");
-			num = scan.nextInt();
-			if (num > random) {
-				System.out.println("DOWN!");
-			}
-			else if(num < random) {
-				System.out.println("UP!");
-			}
-			else {
-				System.out.println("정답입니다!");
+		int n1,n2 = -1;
+		int count = 0;
+		int random;
+		for (int i = 1; i<=100; i++) {
+			random = get_random(min, max);
+			if (random == 50) {
+				count++;
 			}
 		}
+		System.out.println(count);
 	} 
 	public static int get_random(int min, int max) {
 		int random = (int)(Math.random() * (max - min + 1) + min);
