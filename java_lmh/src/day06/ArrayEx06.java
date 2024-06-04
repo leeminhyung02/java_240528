53883c427fc1db7d70e345702537855bab73d293
leeminhyung02 <mi087033@gmail.com>
2024-06-04 14:03:35 +0900

    Update ArrayEx06.java

--- java_lmh/src/day06/ArrayEx06.java
+++ java_lmh/src/day06/ArrayEx06.java
@@ -9,25 +9,25 @@ public class ArrayEx06 {
 		Scanner scan = new Scanner(System.in);
 		int arr[] = new int[4];
 		int total = 0, j = 0;
-		//정석 4개를 입력
+		// 정석 4개를 입력
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print("성적" + (i + 1) + " 입력 :");
 			int input = scan.nextInt();
 			arr[i] = input;
 			total += arr[i];
-		}  //총합을 이용하여 평균을 계산
+		} // 총합을 이용하여 평균을 계산
 		int avg = total / arr.length;
-		for (int tmp : arr) {  //과락 여부를 확인하여 변수에 저장
+		for (int tmp : arr) { // 과락 여부를 확인하여 변수에 저장
 			if (tmp >= 40) {
 				j = 1;
 			} else {
 				j = 0;
 				break;
 			}
-		}
-		if ( j == 1 && avg >=60) {
+		} // 과락이 아니고 평균이 60이상이면 Pass
+		if (j == 1 && avg >= 60) {
 			System.out.println("Pass");
-		}
+		} // 아니면 Fail
 		else {
 			System.out.println("Fail");
 		}
