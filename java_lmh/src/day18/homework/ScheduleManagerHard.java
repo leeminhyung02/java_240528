ebffc26821e3c478e1cc91353c27fc07c4448e2d
leeminhyung02 <mi087033@gmail.com>
2024-06-21 17:51:04 +0900

    day18

--- java_lmh/src/day18/homework/ScheduleManagerHard.java
+++ java_lmh/src/day18/homework/ScheduleManagerHard.java
@@ -6,11 +6,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import program.Program;
 
@@ -21,13 +18,9 @@ public class ScheduleManagerHard implements Program{
 		
 	private final int MAINMEMBER = 1;
 	private final int MAINSH = 2;
-	private final int MAINEXIT = 3;
+	private final int EXIT = 3;
 	
-	private final int INSERT = 1;
-	private final int UPDATE = 2;
-	private final int DELETE= 3;
-	private final int SEARCH = 4;
-	private final int EXIT = 5;
+
 	
 	public void test() {
 		List < Schedule> list = new ArrayList<Schedule>();
@@ -44,6 +37,13 @@ public class ScheduleManagerHard implements Program{
 		System.out.print("메뉴 선택 : ");
 		
 	}
+	public void printmembermanage() {
+		System.out.println("1. 회원 추가");
+		System.out.println("2. 회원 수정");
+		System.out.println("3. 회원 삭제");
+		System.out.println("4. 이전으로");
+		System.out.print("메뉴 선택 : ");
+	}
 	@Override
 	public void printMenu() {
 		System.out.println("메뉴");
@@ -57,22 +57,117 @@ public class ScheduleManagerHard implements Program{
 	}
 	@Override
 	public void runMenu(int menu) throws Exception {
-		switch () {
-		case value: {
-			
-			yield type;
-		}
+		switch (menu) {
+		case MAINMEMBER:    //새로 추가되는 부분  회원관리
+			printmembermanage();
+			int n = scan.nextInt();
+			membermanage(n);
+			break;
+		case MAINSH:          // 기존에 있던 쉬운버전 사용
+			if(checkID()) {
+				ScheduleManager sm = new ScheduleManager();
+				sm.run();
+			}
+			else {
+				System.out.println("등록되지 않은 회원입니다.");
+			}
+			break;
+		case EXIT:
+			System.out.println("프로그램을 종료합니다.");
+			break;
 		default:
-			throw new IllegalArgumentException("Unexpected value: " + key);
+			throw new IllegalArgumentException("잘못된 입력 : " + menu);
 		}
 		
 	}
-	@Override
-	public void run() {
+	private void membermanage(int n) {
+		switch (n) {
+		case 1: //회원 추가
+			addmember();
+			break;
+		case 2: //회원 수정
+			updatemember();
+			break;
+		case 3: //회원 삭제
+			deletemember();
+			break;
+		case 4: // 이전으로
+			System.out.println("메인 메뉴로 돌아갑니다.");
+			break;
+		default:
+			throw new IllegalArgumentException("잘못된 입력 : " + n);
+		}
+		
+	}
+	private void deletemember() {
+
+	}
+	private void updatemember() {
 		// TODO Auto-generated method stub
 		
 	}
-	
+	private void addmember() {
+		System.out.print("아이디 입력 : ");
+		String id = scan.next();
+		String checkfileName = "src/day18/homework/list/"+id+".txt";
+		load(checkfileName);
+		for (Member tmp : member) {
+			if(tmp.getId().equals(id)) {
+				System.out.println("이미 있는 아이디입니다.");
+				return;
+			}
+		}
+		System.out.print("이름 입력 : ");
+		String name = scan.next();
+		Member tmp = new Member(null, id, name);
+		System.out.println("회원생성이 완료되었습니다.");
+		String fileName = "src/day18/homework/list/"+id+".txt";
+		save(fileName);
+	}
+	private boolean checkID() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void run() {
+		
+		int menu = 0;
+		
+		do {
+			MainMenu();
+			menu = scan.nextInt();
+			try {
+				runMenu(menu);
+				
+			}// catch(Exception) {
+		//	}
+			catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("예외발생");
+			}
+		} while (menu != EXIT);
 
+	}
+	
+	@Override
+	public void save(String fileName) {
+		try(FileOutputStream fos = new FileOutputStream(fileName);
+				ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(member);
+		} catch (IOException e) {
+			System.out.println("저장실패");
+		}
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public void load(String fileName) {
+		try(FileInputStream fis = new FileInputStream(fileName);
+				ObjectInputStream ois = new ObjectInputStream(fis)){
+				member = (List<Member>)ois.readObject();
+		} catch (Exception e) {
+			System.out.println("불러오기실패");
+		}
+	}
 
 }
