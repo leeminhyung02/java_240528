8548d98b34ef27e443000304171a908619cffb4d
leeminhyung02 <mi087033@gmail.com>
2024-06-27 08:58:52 +0900

    Update Ex1.java

--- java_lmh/src/ex/Ex1.java
+++ java_lmh/src/ex/Ex1.java
@@ -1,11 +1,29 @@
 package ex;
 
-import java.util.Scanner;
-
 public class Ex1 {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int i = scan.nextInt();
+		int []A = {1,22,3,4,5,6,7};
+		for(int i = 0; i< A.length-1; i++) {
+			if(A[i] >A[i+1]) {
+				int tmp = A[i];
+				A[i] = A[i+1];
+				A[i+1] = tmp;
+			}
+		}
+		for(int tmp : A) {
+			System.out.println(tmp);
+		}
+		for(int i = 0; i< A.length-1; i++) {
+			if(A[i] <A[i+1]) {
+				int tmp = A[i];
+				A[i] = A[i+1];
+				A[i+1] = tmp;
+			}
+		}
+		for(int tmp : A) {
+			System.out.println(tmp);
+		}
+		
 	} 
 	
 }
