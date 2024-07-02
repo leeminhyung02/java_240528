ff9883ef13b6161bcdeb6b42c0aaefe05b05e86c
leeminhyung02 <mi087033@gmail.com>
2024-07-02 11:56:12 +0900

    Update Ex12.java

--- java_lmh/src/day24/Ex12.java
+++ java_lmh/src/day24/Ex12.java
@@ -5,7 +5,7 @@ public class Ex12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*
-		 * 1+2의 결과를 출력하기위해 다믕같이 작성했다
+		 * 1+2의 결과를 출력하기위해 다음같이 작성했다
 		 * 원인 :sum 메소드는 리턴타입이 void임
 		 * 결과 :메소드의 리턴타입을 바꾸거나 int res 없이 단독으로 실행
 		 */
