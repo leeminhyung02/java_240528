37b2a6080177f8d843d307b2dc348ad7717bbdf1
leeminhyung02 <mi087033@gmail.com>
2024-07-08 17:49:52 +0900

    html/css day03

--- java_lmh/src/customer/Room.java
+++ java_lmh/src/customer/Room.java
@@ -9,11 +9,18 @@ import lombok.Data;
 @AllArgsConstructor
 public class Room implements Serializable{
 	private static final long serialVersionUID = 6420217554170896710L;
+	
+	
 	private int room_num;
+	private boolean in;
 	@Override
 	public String toString() {
+		if(in == true) {
+			return "";
+		}
 		return "Room [" + room_num + "]";
 	}
+ 
 	
 	
 }
