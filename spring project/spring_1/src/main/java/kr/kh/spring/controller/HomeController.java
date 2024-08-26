d41ac09ee841a435190c1c09742dda84c0e3cca1
leeminhyung02 <mi087033@gmail.com>
2024-08-26 11:47:36 +0900

    db연동

--- spring project/spring_1/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring project/spring_1/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.kh.controller;
+package kr.kh.spring.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -6,25 +6,27 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.kh.spring.service.MemberService;
+
+@Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private MemberService memberService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		String email = memberService.getEmail("abc123");
+		System.out.println(email);
 		
 		return "home";
 	}
