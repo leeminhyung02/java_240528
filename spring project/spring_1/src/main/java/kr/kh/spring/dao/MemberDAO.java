cfbecd792e190b952f823b5d97fda7534a48f957
leeminhyung02 <mi087033@gmail.com>
2024-08-26 16:46:41 +0900

    update

--- spring project/spring_1/src/main/java/kr/kh/spring/dao/MemberDAO.java
+++ spring project/spring_1/src/main/java/kr/kh/spring/dao/MemberDAO.java
@@ -2,7 +2,13 @@ package kr.kh.spring.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.kh.spring.model.vo.MemberVO;
+
 public interface MemberDAO {
+
+	boolean insertMember(@Param("m")MemberVO member);
+
+	MemberVO selectMember(@Param("me_id")String me_id);
 
 
 }
