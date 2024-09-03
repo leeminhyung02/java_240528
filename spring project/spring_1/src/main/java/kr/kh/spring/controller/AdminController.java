3a97699fe73eaab431bddc29616846dfb04a2ce5
leeminhyung02 <mi087033@gmail.com>
2024-09-03 17:18:27 +0900

    update

--- spring project/spring_1/src/main/java/kr/kh/spring/controller/AdminController.java
+++ spring project/spring_1/src/main/java/kr/kh/spring/controller/AdminController.java
@@ -25,13 +25,35 @@ public class AdminController {
 		model.addAttribute("list", list);
 		return "/admin/community";
 	}
-	@PostMapping("/community/insert")
+	@RequestMapping("/community/insert")
 	public String communityInsert(Model model, String name) {
 		boolean res = postService.insertCommunity(name);
 		if(res) {
 			model.addAttribute("msg", "커뮤니티를 등록했습니다.");
 		}else {
 			model.addAttribute("msg", "커뮤니티를 등록하지 못했습니다.");
+		}
+		model.addAttribute("url", "/admin/community");
+		return "/main/message";
+	}
+	@GetMapping("/community/delete")
+	public String communityDelete(Model model, int co_num) {
+		boolean res = postService.deleteCommunity(co_num);
+		if(res) {
+			model.addAttribute("msg", "커뮤니티를 삭제했습니다.");
+		}else {
+			model.addAttribute("msg", "커뮤니티를 삭제하지 못했습니다.");
+		}
+		model.addAttribute("url", "/admin/community");
+		return "/main/message";
+	}
+	@RequestMapping("/community/update")
+	public String communityUpdate(Model model, CommunityVO community) {
+		boolean res = postService.updateCommunity(community);
+		if(res) {
+			model.addAttribute("msg", "커뮤니티를 수정했습니다.");
+		}else {
+			model.addAttribute("msg", "커뮤니티를 수정하지 못했습니다.");
 		}
 		model.addAttribute("url", "/admin/community");
 		return "/main/message";
