17fe29aee0dfc2d244ede3c26394bf32a4a3fd2d
leeminhyung02 <mi087033@gmail.com>
2024-09-25 17:50:13 +0900

    123

---
+++ spring boot project/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -0,0 +1,46 @@
+package kr.kh.boot.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import kr.kh.boot.model.vo.CommunityVO;
+import kr.kh.boot.model.vo.PostVO;
+import kr.kh.boot.pagination.PageMaker;
+import kr.kh.boot.pagination.PostCriteria;
+import kr.kh.boot.service.PostService;
+import lombok.AllArgsConstructor;
+
+
+@Controller
+@AllArgsConstructor
+public class PostController {
+
+	private PostService postService;
+	
+	@GetMapping("/post/list/{co_num}")
+	public String postList(Model model,@PathVariable int co_num, PostCriteria cri) {
+		cri.setCo_num(co_num);
+		cri.setPerPageNum(4);
+		//커뮤니티 번호에 맞는 게시글 목록을 가져와서 화면에 전달
+		List<PostVO> list = postService.getPostList(cri);
+		//커뮤니티 목록을 가져와서 화면에 전달
+		List<CommunityVO> communities = postService.getCommunityList();
+		PageMaker pm = postService.getPageMaker(cri);
+		model.addAttribute("list", list);
+		model.addAttribute("communities", communities);
+		model.addAttribute("pm", pm);
+		return "post/list";
+	}
+	@GetMapping("/post/detail/{po_num}")
+	public String postDetail(Model model,@PathVariable int po_num) {
+		PostVO post = postService.getPost(po_num);
+		
+		model.addAttribute("post", post);
+		return "post/detail";
+	}
+	
+}
--- spring boot project/boot/src/main/java/kr/kh/boot/dao/PostDAO.java
+++ spring boot project/boot/src/main/java/kr/kh/boot/dao/PostDAO.java
@@ -1,5 +1,18 @@
 package kr.kh.boot.dao;
 
+import java.util.List;
+
+import kr.kh.boot.model.vo.CommunityVO;
+import kr.kh.boot.model.vo.PostVO;
+import kr.kh.boot.pagination.PostCriteria;
+
 public interface PostDAO {
-	int count();
+	
+	List<PostVO> selectPostList(PostCriteria cri);
+
+	List<CommunityVO> selectCommunityList();
+
+	int selectCountPostList(PostCriteria cri);
+
+	PostVO selectPost(int po_num);
 }
