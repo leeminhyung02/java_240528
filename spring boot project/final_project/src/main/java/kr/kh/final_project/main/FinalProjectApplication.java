016859f367c769307d7e050b1a19bdd38de68089
leeminhyung02 <mi087033@gmail.com>
2024-09-26 17:06:53 +0900

    프로젝트-생성

---
+++ spring boot project/final_project/src/main/java/kr/kh/final_project/main/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.kh.final_project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
